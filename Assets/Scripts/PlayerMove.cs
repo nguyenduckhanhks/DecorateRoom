6a064ea7d5376f24d47ed0c5054d0c6fc09f8ce5
Nguyen Duc Khanh <nguyenduckhanhks@gmail.com>
2020-11-21 17:02:28 +0700

    init

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public static PlayerMove playerMove;
+    private CharacterController controller;
+    public float playerSpeed = 2.0f;
+    public float jumpHeight = 1.0f;
+
+    private void Start()
+    {
+        controller = gameObject.AddComponent<CharacterController>();
+        if(playerMove == null)
+            playerMove = this.gameObject.GetComponent<PlayerMove>();
+    }
+
+    void Update()
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        gameObject.transform.Translate(move * Time.deltaTime * playerSpeed);
+    }
+}
---
+++ Assets/Scripts/TmpEntity.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TmpEntity : MonoBehaviour
+{
+    private bool isTrigger = false;
+    private bool isPictureOnWall = true;
+    private MeshRenderer meshRender;
+    public GameObject Entity;
+    public bool IsTrigger {
+        set { isTrigger = value; }
+        get { return isTrigger; }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(GetComponent<MeshCollider>() == null) {
+            gameObject.AddComponent<MeshCollider>();
+        }
+        GetComponent<MeshCollider>().convex = true;
+        GetComponent<MeshCollider>().isTrigger = true;
+        meshRender = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.rotation = Quaternion.LookRotation(new Vector3(0,1,0));
+        if(this.tag == "TmpPicture") {
+            if(Player.player.IsChooseWall) {
+                isPictureOnWall = true;
+                this.transform.position = Player.player.ChoosedWall.point;
+                this.transform.rotation = Player.player.ChoosedWall.collider.gameObject.GetComponent<Transform>().rotation;
+            } else {
+                isPictureOnWall = false;
+                resetPosition();
+            }
+        }
+        if(isTrigger || !isPictureOnWall) {
+            meshRender.material = GameController.gameController.MaterialEntity[GameController.indexErr];
+        } else {
+            meshRender.material = GameController.gameController.MaterialEntity[GameController.indexSuccess];
+        }
+        if (Input.GetButtonDown("Fire1") && !GameController.gameController.EditMode && !GameController.gameController.ExitMode && !GameController.gameController.MenuMode)
+            renderEntity();
+        if (Input.GetButtonDown("Quit") && !GameController.gameController.EditMode && !GameController.gameController.ExitMode && !GameController.gameController.MenuMode)
+            Destroy(gameObject);
+    }
+
+    private void OnTriggerStay(Collider other) {
+        if(other.tag == "Entity" || other.tag == "Picture") {
+            isTrigger = true;
+        }
+        if(this.tag != "TmpPicture") {
+            if(other.tag == "Wall") {
+                isTrigger = true;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if(other.tag == "Entity" || other.tag == "Picture") {
+            isTrigger = false;
+        }
+        if(this.tag != "TmpPicture") {
+            if(other.tag == "Wall") {
+                isTrigger = false;
+            }
+        }
+    }
+
+    public void renderEntity(){
+        if(!isTrigger){
+            if(this.tag == "TmpPicture") {
+                if(!Player.player.IsChooseWall) {
+                    return;
+                }
+            }
+            GameObject entity = Instantiate(Entity, this.gameObject.transform.position, Quaternion.identity);
+            entity.GetComponent<Transform>().rotation = this.transform.rotation;
+        }
+    }
+
+    public void resetPosition() {
+        this.transform.localPosition = GameController.gameController.DefaultTmpEntityPosition;
+    }
+}
